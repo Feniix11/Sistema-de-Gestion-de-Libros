a4b7380c618bc978e93c33ad425463229e3bbb80
Felix <feliixcastro11@gmail.com>
2024-10-23 11:12:36 -0700

    Conexion a docker MySQL
    Se declararon las variables de entorno y la conexion con TypeORM

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql', // Tipo de base de datos
+      host: process.env.HOST_FELIX, // Dirección del host
+      port: Number(process.env.PORT_DB), // Puerto de MySQL (por defecto es 3306)
+      username: process.env.USERNAME, // Usuario de la base de datos
+      password: process.env.PASSWORD, // Contraseña de la base de datos
+      database: process.env.DB, // Nombre de la base de datos
+      autoLoadEntities: true, // Aquí incluyes las entidades
+      synchronize: true, // Sincroniza automáticamente las entidades con la base de datos (desactívalo en producción)
+    }),
+  ],
+
   controllers: [AppController],
   providers: [AppService],
 })
